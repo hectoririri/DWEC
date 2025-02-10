2ce3f84050f3374651b673d3b4722ac55981a212
hectoririri <hecnugar@gmail.com>
2025-02-10 10:22:56 +0100

    probando fetch

--- public/js/utiles.js
+++ public/js/utiles.js
@@ -1,34 +1,27 @@
-$(document).ready(function () {
-    // Cargamos la tabla en formato datatable
-    // Ajax para cargar la tabla
-    $.ajax({
-        url: 'php/cargarVehiculos.php',
-        type: 'POST',
-        success: function (respuesta) {
-            console.log(respuesta);
-            let tabla = $('#tabla_vehiculos');
-            let thead = $('<thead></thead>');
-            let tr = $('<tr></tr>');
-            for (const key in respuesta[0]) {
-                tr.append($('<th></th>').text(key).css('text-align', 'center'));
+let app = new Vue({
+    el: "#app", // Elemento del DOM al que hace referencia
+    data: {
+        cuerpo: "", // Variable para almacenar el HTML dinámico
+        email: "", // Modelo para el campo email
+        contrasena: "", // Modelo para el campo contraseña
+        logeado: false,
+    },
+    methods: {
+        intentoLogin() {
+            fetch('http://localhost/todos/usuarios')
+                .then(response => response.json())
+                .then(user => {
+                    if (user){
+                        console.log(user);
+                    }
+                })
+            if (this.email === 'admin' && this.contrasena === 'admin') {
+                this.logeado = true;
+                console.log("logeado");
+            } else {
+                this.logeado = false;
+                console.log("no logeado");
             }
-            tr.append($('<th></th>').text("Eliminar"));
-            tabla.append(thead.append(tr));
-
-            let tbody = $('<tbody></tbody>');
-            respuesta.forEach(element => {
-                let tr = $('<tr></tr>');
-                for (const key in element) {
-                    let td = $('<td></td>').text(element[key]);
-                    tr.append(td);
-                }
-                tbody.append(tr);
-            });
-            let td = $('<td></td>').append($('<button></button>').text('Eliminar').addClass('btn btn-warning').attr('data-id', element.id));
-            tr.append(td);
-            tbody.append(tr);
-            tabla.append(tbody);
-            tabla.DataTable();
         }
-    });
+    }
 });
